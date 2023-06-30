d1c606d00ed8743d7182f030249ef5ca0a210e51
Iain <iain.carr@questionablequality.com>
2023-06-30 16:32:34 +0100

    new startup messages

--- client/components/header.js
+++ client/components/header.js
@@ -22,7 +22,7 @@ export default ({ currentUser }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <Link className="navbar-brand" href="/">
-        GitTip
+        Just the tip
       </Link>
 
       <div className="d-flex justify-content-end">
